c3716cfb30c147928ea125c69d9f9e7ca28a44f0
lifewawj <arevaloaj01@gmail.com>
2024-01-18 22:04:32 -0800

    Installed ExpressJS, Added index.js and index.html with Hello World syntax, Added .gitignore file, Added an .env file

---
+++ server.js
@@ -0,0 +1,24 @@
+// NPM Packages included
+const express = require('express');
+const path = require('path');
+
+// Create an app variable and set it to express to initialize the server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+});
+
+app.get('/paths', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/paths.html'))
+);
+
+// Inits the server, and sets up our server to listen for http requests
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
