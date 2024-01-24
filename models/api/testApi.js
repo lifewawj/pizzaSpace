30fa883c44484b4dcd66684a057e4a4b95cd7de2
Shibiiii <purplejohn13@gmail.com>
2024-01-23 19:19:45 -0800

    revised api tests call

--- models/api/testApi.js
+++ models/api/testApi.js
@@ -6,39 +6,32 @@ const API_KEY = 'api_key=121eab3cd80c3bca77d2becee94b9536';
 let SEARCH_URL =
   BASE_URL + 'search/movie?' + API_KEY + '&sort_by=popularity.desc&query=';
 
-function searchMovie() {
-  let f = document.getElementById('search_movie');
-  f.addEventListener('submit', () => {
-    let user_input = search_input.value;
-    if (user_input && user_input.trim() != '') {
-      let query = SEARCH_URL + user_input;
-      console.log();
-      getMovies(query);
-    }
-  });
-}
-
 function getMovies(my_api) {
-  main.innerHTML = '';
-  fetch(my_api, {
+  return fetch(my_api, {
     method: 'GET',
     cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: { 'Content-Type': 'application/json' },
   })
     .then((res) => res.json())
-    .then((data) => {
-      let arr = data.results;
-      if (arr && arr.length > 0) {
-        arr.forEach((movie) => {
-          addMovie(movie);
-        });
-      } else {
-        getMovies(MOVIE_URL, DEFAULT_PAGE);
-        console.log("Can't find results");
-      }
-    })
     .catch((err) => {
       console.log(err.message);
     });
 }
+
+function renderMovies(res) {
+  let ul = document.getElementById('results');
+  ul.innerHTML = '';
+  res.results.forEach((result) => {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(result.title));
+    ul.appendChild(li);
+  });
+}
+
+let f = document.getElementById('search_movie');
+f.addEventListener('click', () => {
+  let user_input = search_input.value;
+  if (user_input && user_input.trim() != '') {
+    let query = SEARCH_URL + user_input;
+    getMovies(query).then(renderMovies);
+  }
+});
