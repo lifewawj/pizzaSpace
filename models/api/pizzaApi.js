599b0899ed6e1ae0b5a2d0beb5339568e82acf34
John <purplejohn13@gmail.com>
2024-01-28 14:04:53 -0800

    set up base url and api key

--- models/api/pizzaApi.js
+++ models/api/pizzaApi.js
@@ -1,1 +1,7 @@
 // API Key: 549304fb09a74a5bb4ab89b9d72c70c1
+// Request Random Url: https://api.spoonacular.com/recipes/random?number=1&include-tags=pizza
+// Base Url: https://api.spoonacular.com/recipes/random
+
+const BASE_URL =
+  'https://api.spoonacular.com/recipes/random?number=1&include-tags=pizza';
+const API_KEY = '549304fb09a74a5bb4ab89b9d72c70c1';
