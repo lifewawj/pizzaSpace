f43968a1743c54cc25f624f85b30ad3f9f53fbe7
John <purplejohn13@gmail.com>
2024-01-28 19:05:27 -0800

    renamed variables

--- models/api/pizzaApi.js
+++ models/api/pizzaApi.js
@@ -2,9 +2,9 @@
 // Request Random Url: https://api.spoonacular.com/recipes/random?number=1&include-tags=pizza
 // Base Url: https://api.spoonacular.com/recipes/random
 
-const BASE_URL =
+const API_URL =
   'https://api.spoonacular.com/recipes/random?number=1&include-tags=pizza';
-const API_KEY = '549304fb09a74a5bb4ab89b9d72c70c1';
+const KEY = '549304fb09a74a5bb4ab89b9d72c70c1';
 
 function randomPizza(my_api) {
   return fetch(my_api, {
