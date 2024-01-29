df401f6c8cc93829e895d177df99378c66bcfa9f
John <purplejohn13@gmail.com>
2024-01-28 19:13:22 -0800

    edited api call

--- models/api/pizzaApi.js
+++ models/api/pizzaApi.js
@@ -6,8 +6,8 @@ const API_URL =
   'https://api.spoonacular.com/recipes/random?number=1&include-tags=pizza';
 const KEY = '549304fb09a74a5bb4ab89b9d72c70c1';
 
-function randomPizza(my_api) {
-  return fetch(my_api, {
+function randomPizza(API_URL) {
+  return fetch(API_URL, {
     method: 'GET',
     cache: 'no-cache',
   })
